621c55b9f75a034493a2271701422f4846b1f31c
Ayushman Singh Chauhan <ascb508@gmail.com>
2023-09-25 01:37:44 +0530

    [➕] `spring_initializr`
    Initial Spring Boot App

---
+++ src/main/java/com/gadaElectronics/GadaElectronicsApplication.java
@@ -0,0 +1,13 @@
+package com.gadaElectronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadaElectronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadaElectronicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gadaElectronics/GadaElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadaElectronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadaElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
